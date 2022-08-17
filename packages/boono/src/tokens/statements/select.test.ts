7cadd51dba28761c30a6019316e53e2a47138b8f
Sergey Popov <quolpr@gmail.com>
2022-08-17 19:04:33 +0300

    Add initial window fn support

---
+++ packages/boono/src/tokens/statements/select.test.ts
@@ -0,0 +1,20 @@
+import { sql } from "@kikko-land/sql";
+import { describe, expect, it } from "vitest";
+
+import { select } from "./select";
+
+describe("select", () => {
+  it("works", () => {
+    expect(
+      select()
+        .from("notes")
+        .toSql().raw
+    ).to.eq('SELECT * FROM "notes"');
+
+    expect(
+      select({ a: sql`LOWER(X)` })
+        .from("notes")
+        .toSql().raw
+    ).to.eq('SELECT LOWER(X) AS "a" FROM "notes"');
+  });
+});
